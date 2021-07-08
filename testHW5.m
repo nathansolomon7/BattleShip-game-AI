d8208f0459daed37280a486f4caa7d61bc444cec
nathansolomon7 <85477782+nathansolomon7@users.noreply.github.com>
2021-07-07 23:50:12 -0700

    Add files via upload

---
+++ testHW5.m
@@ -0,0 +1,22 @@
+% People on this team: Nathan Solomon and Luke Botsford 
+
+ playGameHW5(10,'n',1)
+% playGameHW5(100,'r',100)
+% playGameHW5(10,'n',100)
+% playGameHW5(100,'n',100)
+
+% For our test run of a 10x100 gridsize for 100 iterations of our random
+% search function, we had an average of 94.8 queries. 
+% For our test run of a 100x100 gridsize for 10 iterations of our random
+% search function, we had an average of 9441 queries. 
+% For our test run of a 10x100 gridsize for 100 iterations of our
+% neighborhood search function, we had an average of 65.47 queries. 
+% For our test run of a 100x100 gridsize for 100 iterations of our
+% neighborhood search function, we had an average of 5187.87 queries. 
+
+
+
+
+
+
+
